1da30c350e65ee73d01dc2fef895e7c11c046221
Ikhwal17 <134153912+Ikhwal17@users.noreply.github.com>
2025-04-30 09:28:38 +0700

    website personal portfolio Muhammad Ikhwal
    Menambahkan file HTML, CSS, dan gambar untuk halaman utama website pribadi Muhammad Ikhwal. Website ini dirancang sebagai portofolio online untuk menampilkan latar belakang, keterampilan, dan proyek pengembangan web.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle Class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik di luar slidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
